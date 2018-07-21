fb026577d132b1e55cb08f303e2824dcd95aa3c6
stonesfour <sunshilei@caicloud.io>
2018-07-21 23:18:56 +0800

    cloud 组件验证

---
+++ microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -0,0 +1,38 @@
+package com.itmuch.cloud.controller;
+
+import com.itmuch.cloud.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.itmuch.cloud.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RestController
+public class MovieController {
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @Autowired
+  private UserFeignClient userFeignClient;
+
+  @GetMapping("/movie/{id}")
+  public User findByIdFeign(@PathVariable Long id) {
+    return this.userFeignClient.findById(id);
+  }
+
+
+  @GetMapping("/movie/address/{id}")
+  @HystrixCommand(fallbackMethod = "findByIdFallback")
+  public User findById(@PathVariable Long id) {
+    return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
+  }
+
+  public User findByIdFallback(Long id) {
+    User user = new User();
+    user.setId(0L);
+    return user;
+  }
+}
---
+++ microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/itmuch/cloud/feign/UserFeignClient.java
@@ -0,0 +1,12 @@
+package com.itmuch.cloud.feign;
+import com.itmuch.cloud.entity.User;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "microservice-provider-user", /*fallback = HystrixClientFallback.class, */fallbackFactory = HystrixClientFactory.class)
+public interface UserFeignClient {
+  @RequestMapping(value = "/simple/{id}", method = RequestMethod.GET)
+  public User findById(@PathVariable("id") Long id);
+}

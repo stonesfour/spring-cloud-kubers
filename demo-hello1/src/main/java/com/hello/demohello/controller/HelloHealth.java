e0d7bf4c5a6d91e5c57fecc686587e7be7b05653
stonesfour <sunshilei@caicloud.io>
2018-07-26 16:46:24 +0800

    update init

---
+++ demo-hello1/src/main/java/com/hello/demohello/controller/HelloHealth.java
@@ -0,0 +1,31 @@
+package com.hello.demohello.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @PACKAGE_NAME：com.hello.demohello.controller
+ * @DATE: 2018/7/24
+ * @AURH: shilei
+ * @DESC:
+ */
+@RestController
+public class HelloHealth {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/health/{name}")
+    public String health(@PathVariable("name") String name){
+
+        return "health: "+name;
+    }
+
+    @GetMapping("/movie/address/{id}")
+    public String findById(@PathVariable Long id) {
+        return this.restTemplate.getForObject("http://demo-hello/health/关联项目+" + id, String.class);
+    }
+}

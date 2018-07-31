ea526132691830aaa5a9c9cece4958668093164a
Shilei Sun <sunshilei@caicloud.io>
2018-07-31 09:25:50 +0800

    Update MovieController.java

--- microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/itmuch/cloud/controller/MovieController.java
+++ microservice-consumer-movie-ribbon-with-hystrix/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -18,16 +18,27 @@ public class MovieController {
   @Autowired
   private UserFeignClient userFeignClient;
 
-  @GetMapping("/movie/{id}")
+  @GetMapping("/user/{id}")
   public User findByIdFeign(@PathVariable Long id) {
     return this.userFeignClient.findById(id);
   }
 
 
-  @GetMapping("/movie/address/{id}")
+  @GetMapping("/user/info/{id}")
   @HystrixCommand(fallbackMethod = "findByIdFallback")
   public User findById(@PathVariable Long id) {
     return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
+  }
+
+  @GetMapping("/kuber/back/{id}")
+  @HystrixCommand(fallbackMethod = "findByIdFallback")
+  public String findByKuber(@PathVariable Long id) {
+    return this.restTemplate.getForObject("http://demo-hello:8080/health/kuber+" + id, String.class);
+  }
+
+  @GetMapping("/kuber/{id}")
+  public String findByKuberParam(@PathVariable Long id) {
+    return this.restTemplate.getForObject("http://demo-hello:8080/health/kuber+" + id, String.class);
   }
 
   @GetMapping("/simple/{id}")
@@ -37,17 +48,18 @@ public class MovieController {
 
   @GetMapping("/sleep/{times}")
   public Long findSleep(@PathVariable Long times) {
-    new Thread() {
-      public void run() {
-        try {
-          Thread.sleep(times);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-      }
-    }.start();
+
+    long a =System.currentTimeMillis();
+    try {
+      Thread.sleep(times);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println((System.currentTimeMillis()-a)/1000);
+
     return times;
   }
+
 
   public User findByIdFallback(Long id) {
     User user = new User();
